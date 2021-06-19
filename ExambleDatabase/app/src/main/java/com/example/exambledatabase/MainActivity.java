3cee103c6af163a570ce894ddf2d96728464713e
chaitanyak963 <you@example.com>
2021-06-19 17:45:35 +0530

    19-06-2021

--- ExambleDatabase/app/src/main/java/com/example/exambledatabase/MainActivity.java
+++ ExambleDatabase/app/src/main/java/com/example/exambledatabase/MainActivity.java
@@ -15,12 +15,15 @@ import com.example.exambledatabase.RDB.RTable;
 import com.example.exambledatabase.RDB.RViewModel;
 import androidx.lifecycle.ViewModelProviders;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText roll,name,num;
     RecyclerView rv;
     static RViewModel rViewModel;
+    List<RTable> list;
+    String l="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         rViewModel.readData().observe(this, new Observer<List<RTable>>() {
             @Override
             public void onChanged(List<RTable> rTables) {
+                list = rTables;
                 MyAdapter adapter = new MyAdapter(MainActivity.this,rTables);
                 rv.setAdapter(adapter);
                 rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
@@ -42,11 +46,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void save(View view) {
         /*In this Method we implemented Insert operation on RDB*/
+        for (int i =0;i<list.size();i++){
+            l = list.get(i).getSroll();
+        }
         RTable rTable = new RTable();
-        rTable.setSroll(roll.getText().toString());
-        rTable.setSname(name.getText().toString());
-        rTable.setSnumber(num.getText().toString());
-        rViewModel.insert(rTable);
-        Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
+        String r = roll.getText().toString();
+        String n = name.getText().toString();
+        String nu = num.getText().toString();
+        if (r.isEmpty() | n.isEmpty() | nu.isEmpty()) {
+            Toast.makeText(this, "Please fill all the details",
+                    Toast.LENGTH_SHORT).show();
+        } else if(l.equals(r)) {
+            Toast.makeText(this, "Duplicate Data", Toast.LENGTH_SHORT).show();
+        }else{
+            rTable.setSroll(r);
+            rTable.setSname(n);
+            rTable.setSnumber(nu);
+            rViewModel.insert(rTable);
+            Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
+        }
     }
 }
